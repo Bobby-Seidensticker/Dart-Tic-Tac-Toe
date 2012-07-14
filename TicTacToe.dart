33f89295db5bd051117b0f6b70d84f71c56a0e6a
Bobby Seidensticker <bseidensticker@gmail.com>
2012-07-14 11:34:32 -0700

    can draw an x, put everything in main

--- TicTacToe.dart
+++ TicTacToe.dart
@@ -1,16 +1,15 @@
 #import('dart:html');
 
-void main() {
-  showMessage('Welcome to Dart!');
-}
-
 final int SIZE = 3;
 final int SQUARE_SIZE = 100;
+List<String> PLAYER_COLORS;
 
-void showMessage(String message) {
-  /*var textElement = query('#text');
 
-  textElement.text = message;*/
+
+void main() {
+  PLAYER_COLORS = new List<String>();
+  PLAYER_COLORS.add('#f00');
+  PLAYER_COLORS.add('#00f');
   
   CanvasElement board = query('#board');
   var ctx = board.context2d;
@@ -21,11 +20,24 @@ void showMessage(String message) {
     ctx.lineTo(SIZE * SQUARE_SIZE, SQUARE_SIZE * i - 0.5);
     ctx.stroke();
   }
-  
+  drawX(board, 0, 0, 0);
 }
 
-bool drawX(CanvasRenderingContext2d ctx, int x, int y, String color) {
+bool drawX(CanvasElement board, int x, int y, int player) {
   if (x >= SIZE || y >= SIZE || x < 0 || y < 0) {
     return false;
   }
+  CanvasRenderingContext2D ctx = board.context2d;
+  if (player == 0) {
+    ctx.strokeStyle = "#00f";
+  } else if (player == 1) {
+    ctx.strokeStyle = '#f00';
+  } else {
+    return false;
+  }
+  ctx.moveTo(SQUARE_SIZE * x, SQUARE_SIZE * y);
+  ctx.lineTo(SQUARE_SIZE * (x + 1), SQUARE_SIZE * (y + 1));
+  ctx.moveTo(SQUARE_SIZE * (x + 1), SQUARE_SIZE * y);
+  ctx.lineTo(SQUARE_SIZE * x, SQUARE_SIZE * (y + 1));
+  ctx.stroke();
 }
